ea33cea2b6aa6dcd1cc09d348b2454274dc0b11a
Milagros Sassenus <milu125000@gmail.com>
2022-05-26 17:39:29 -0300

    Cambio color

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import '../styles.css';
 function Footer() {
   return (
     <div className="div-footer">
-      <footer class="py-5 bg-dark">
+      <footer class="py-5 bg-warning">
         <div class="container">
           <p class="m-0 text-center text-white">
             Copyright &copy; Mc Donals Web 2022
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,11 +3,11 @@ import '../styles.css';
 function Header() {
   return (
     <div className="div-header">
-        <header class="bg-dark py-5">
+        <header class="bg-warning py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
                     <h1 class="display-4 fw-bolder">McDonals Argentina</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laboriosam dicta consectetur mollitia minus aperiam iste quo! Dicta, consequuntur nemo!</p>
+                    <p class="lead fw-normal text-white-70">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laboriosam dicta consectetur mollitia minus aperiam iste quo! Dicta, consequuntur nemo!</p>
                 </div>
             </div>
         </header>
